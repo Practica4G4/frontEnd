d3aa277809584823621079a52626da90c26a5ccc
Julio Cotzojay <juliocotzo11@gmail.com>
2020-04-22 02:00:47 -0600

    Tipo de Cambio

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,19 +1,34 @@
-import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { componentFactoryName } from '@angular/compiler';
-import { RegistroComponent } from './components/registro/registro.component';
-import { LoginComponent } from './components/login/login.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import {NgModule, Component} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {componentFactoryName} from '@angular/compiler';
+import {RegistroComponent} from './components/registro/registro.component';
+import {LoginComponent} from './components/login/login.component';
+import {PerfilComponent} from './components/perfil/perfil.component';
+import {TipoDeCambioComponent} from './components/tipo-de-cambio/tipo-de-cambio.component';
 
 
 const routes: Routes = [
-  { path:'registro',component: RegistroComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'perfil', component: PerfilComponent }
+  {
+    path: 'registro',
+    component: RegistroComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'perfil',
+    component: PerfilComponent
+  },
+  {
+    path: 'tipo-de-cambio',
+    component: TipoDeCambioComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
