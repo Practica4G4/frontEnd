84a407257b7a5de4e20272aab704c707b0dc3f80
Fernando Flores <nbafernandoflores8@gmail.com>
2020-05-02 20:34:09 -0600

    isNumber() validation function add

--- frontEnd/src/app/components/registro/registro.component.ts
+++ frontEnd/src/app/components/registro/registro.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { RegistroUsuario } from '../../models/usuario/registro/registro-usuario';
-import { NgForm } from '@angular/forms';
-import { IfStmt } from '@angular/compiler';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RegistroUsuario} from '../../models/usuario/registro/registro-usuario';
+import {NgForm} from '@angular/forms';
+import {IfStmt} from '@angular/compiler';
+import {Router} from '@angular/router';
 import {UsuarioService} from '../../service/usuario/usuario.service';
 
 
-
 declare var M: any;
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
@@ -16,20 +16,22 @@ declare var M: any;
 export class RegistroComponent implements OnInit {
 
   usuarioSeleccionado = new RegistroUsuario();
-  constructor(private service: UsuarioService,private router: Router) { }
+
+  constructor(private service: UsuarioService, private router: Router) {
+  }
 
   ngOnInit(): void {
     this.usuarioSeleccionado = new RegistroUsuario();
   }
 
-  regitrarUsuario(form: NgForm){
-    if(form.value.dpi){
-      this.service.postRegistrarUsuario(form.value).subscribe(res =>{
+  regitrarUsuario(form: NgForm) {
+    if (form.value.dpi) {
+      this.service.postRegistrarUsuario(form.value).subscribe(res => {
         M.toast({html: 'Usuario Creado'});
         this.router.navigate(['/login']);
 
       }, (err: any) => {
-        if(err.status == 400){
+        if (err.status == 400) {
           M.toast({html: 'dpi o numero de cuenta ya estan en uso.'});
           this.resetForm(form);
         }
@@ -37,11 +39,19 @@ export class RegistroComponent implements OnInit {
     }
   }
 
-  resetForm(form?: NgForm){
-    if(form){
+  resetForm(form?: NgForm) {
+    if (form) {
       form.reset();
       this.usuarioSeleccionado = new RegistroUsuario();
     }
   }
+
+
+  isNumber(value: string | number): boolean {
+    return ((value != null) &&
+      (value !== '') &&
+      !isNaN(parseInt(value.toString(), 10)));
+  }
+
 
 }
