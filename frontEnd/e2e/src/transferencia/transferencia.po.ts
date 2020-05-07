33cc270b5494ca695b7e2ef452ccdfe7753c4ff0
Fernando Flores <nbafernandoflores8@gmail.com>
2020-05-06 21:16:47 -0600

    transferencia functional tests add

---
+++ frontEnd/e2e/src/transferencia/transferencia.po.ts
@@ -0,0 +1,36 @@
+import {browser, by, element} from 'protractor';
+
+export class TransferenciaComponent {
+  navigateTo() {
+    return browser.get('/transferencia');
+  }
+
+  getTransferenciaTitle() {
+    return element(by.id('transferencia-title'));
+  }
+
+  getDatosPersonalesEncabezado() {
+    return element(by.id('datos-personales'));
+  }
+
+  getDatosTransferenciaEncabezado() {
+    return element(by.id('datos-transferencia'));
+  }
+
+  getSelectCuentabiente() {
+    return element(by.id('slc-cuentabiente'));
+  }
+
+  getSelectCuentabienteSelected() {
+    return element(by.id('slc-cuentabiente option:selected'));
+  }
+
+  getMontoTransferencia() {
+    return element(by.id('monto'));
+  }
+
+  getBtnTransferir() {
+    return element(by.css('button.btn.btn-success.btn-block'));
+  }
+
+}
